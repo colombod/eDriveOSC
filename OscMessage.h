3b38ea37e2b5ac92c7995dad45682046467c6f12
Diego Colombo <diego@mubreeze.com>
2013-08-17 13:09:32 +0100

    Initial include

---
+++ OscMessage.h
@@ -0,0 +1,20 @@
+//
+//  OscMessage.h
+//  eDrive
+//
+//  Created by Diego Colombo on 07/07/2013.
+//
+//
+
+#ifndef eDrive_OscMessage_h
+#define eDrive_OscMessage_h
+
+namespace mUbreeze{
+    namespace eDrive{
+        class OscMessage{
+            
+        };
+    }
+}
+
+#endif
